2fc42680abf1f83149775f115ed0b6f9dbba947e
daprietoa <davidfernandoprieto@gmail.com>
2021-11-14 12:41:05 -0500

    updated

---
+++ src/models/empleado.model.ts
@@ -0,0 +1,64 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Empleado extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  Id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Nombres: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Apellidos: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  TelefonoCelular: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Direccion: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  FechaNacimiento: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Rol: string;
+
+
+  constructor(data?: Partial<Empleado>) {
+    super(data);
+  }
+}
+
+export interface EmpleadoRelations {
+  // describe navigational properties here
+}
+
+export type EmpleadoWithRelations = Empleado & EmpleadoRelations;
