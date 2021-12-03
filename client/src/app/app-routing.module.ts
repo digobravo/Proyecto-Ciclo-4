af4d306e7d63af011f917cdf2b1938d65f99dc08
Diego Bravo A <digobravo@outlook.com>
2021-12-03 14:00:34 -0500

    División por modulos de los diversos componentes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SeguridadModule } from './modulos/seguridad/seguridad.module';
+import { ErrorComponent } from './plantilla/error/error.component';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inicio',
+    component: InicioComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/inicio',
+  },
+  {
+    path: 'seguridad',
+    loadChildren: () =>
+      import('./modulos/seguridad/seguridad.module').then(
+        (x) => x.SeguridadModule
+      ),
+  },
+  {
+    path: 'administracion',
+    loadChildren: () =>
+      import('./modulos/administracion/administracion.module').then(
+        (x) => x.AdministracionModule
+      ),
+  },
+  {
+    path: 'inmuebles',
+    loadChildren: () =>
+      import('./modulos/inmuebles/inmuebles.module').then(
+        (x) => x.InmueblesModule
+      ),
+  },
+  {
+    path: '**',
+    component: ErrorComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- client/src/app/modulos/administracion/administracion-routing.module.ts
+++ client/src/app/modulos/administracion/administracion-routing.module.ts
@@ -1,7 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuscarInmuebleComponent } from './inmuebles/buscar-inmueble/buscar-inmueble.component';
+import { CrearInmuebleComponent } from './inmuebles/crear-inmueble/crear-inmueble.component';
+import { EditarInmuebleComponent } from './inmuebles/editar-inmueble/editar-inmueble.component';
+import { EliminarInmuebleComponent } from './inmuebles/eliminar-inmueble/eliminar-inmueble.component';
+import { BuscarPersonaComponent } from './personas/buscar-persona/buscar-persona.component';
+import { CrearPersonaComponent } from './personas/crear-persona/crear-persona.component';
+import { EditarPersonaComponent } from './personas/editar-persona/editar-persona.component';
+import { EliminarPersonaComponent } from './personas/eliminar-persona/eliminar-persona.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'crear-persona',
+    component: CrearPersonaComponent
+  },
+  {
+    path: 'editar-persona',
+    component: EditarPersonaComponent
+  },
+  {
+    path: 'eliminar-persona',
+    component: EliminarPersonaComponent
+  },
+  {
+    path: 'buscar-persona',
+    component: BuscarPersonaComponent
+  },
+  {
+    path: 'crear-inmueble',
+    component: CrearInmuebleComponent
+  },
+  {
+    path: 'editar-inmueble',
+    component: EditarInmuebleComponent
+  },
+  {
+    path: 'eliminar-inmueble',
+    component: EliminarInmuebleComponent
+  },
+  {
+    path: 'buscar-inmueble',
+    component: BuscarInmuebleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
